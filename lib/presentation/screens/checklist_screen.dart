e7a5545ec3d15e0f89179f79b20d89529e16c4c3
Thean Yee Sin <yeesinthean@outlook.com>
2025-07-28 18:06:00 +0800

    task: initial checklist screen

--- lib/presentation/screens/checklist_screen.dart
+++ lib/presentation/screens/checklist_screen.dart
@@ -10,9 +10,6 @@ class ChecklistScreen extends StatefulWidget {
 class _ChecklistScreenState extends State<ChecklistScreen> {
   @override
   Widget build(final BuildContext context) => Scaffold(
-    appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: const Text("FLUTTER DEMO CHECKLIST PAGE"),
-    ),
+    appBar: AppBar(title: const Text("Checklist"), centerTitle: true),
   );
 }
