977666072f159754f9bdcd47d01a47c7fab27cb1
Thean Yee Sin <yeesinthean@outlook.com>
2026-08-04 19:45:21 +0800

    chore: created home feature with home screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ import "package:tabemashou/presentation/screens/about_screen.dart";
 import "package:tabemashou/presentation/screens/main_navigator_screen.dart";
 import "package:tabemashou/presentation/screens/settings/settings_screen.dart";
 import "package:tabemashou/presentation/screens/tabs/categories_screen.dart";
-import "package:tabemashou/presentation/screens/tabs/home_screen.dart";
+import "package:tabemashou/features/home/screens/home_screen.dart";
 import "package:tabemashou/presentation/screens/tabs/more_screen.dart";
 import "package:tabemashou/presentation/screens/tabs/random_restaurant_screen.dart";
 
--- lib/presentation/screens/main_navigator_screen.dart
+++ lib/presentation/screens/main_navigator_screen.dart
@@ -3,7 +3,7 @@ import "package:tabemashou/core/constants/path.dart";
 import "package:tabemashou/core/type/common.dart";
 import "package:tabemashou/presentation/screens/tabs/categories_screen.dart";
 import "package:tabemashou/features/checklist/presentation/screens/home_checklist_screen.dart";
-import "package:tabemashou/presentation/screens/tabs/home_screen.dart";
+import "package:tabemashou/features/home/screens/home_screen.dart";
 import "package:tabemashou/presentation/screens/tabs/more_screen.dart";
 import "package:tabemashou/presentation/screens/tabs/random_restaurant_screen.dart";
 import "package:tabemashou/core/widgets/custom_nav_bar.dart";
