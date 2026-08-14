cafeb0b6f73e726bd56047e25ac2815105870af6
Thean Yee Sin <yeesinthean@outlook.com>
2026-08-14 17:39:45 +0800

    chore: update theme color

--- lib/core/theme/custom_themes/app_bar_theme.dart
+++ lib/core/theme/custom_themes/app_bar_theme.dart
@@ -1,16 +1,34 @@
 import "package:flutter/material.dart";
+import "package:tabemashou/core/theme/custom_themes/app_colors.dart";
 
 class TAppBarTheme {
   TAppBarTheme._();
 
-  static AppBarTheme lightAppBarTheme() => AppBarTheme(
-    backgroundColor: Colors.grey[300],
-    elevation: 2,
-    foregroundColor: Colors.black,
-  ).copyWith();
+  static AppBarTheme lightAppBarTheme() => const AppBarTheme(
+    backgroundColor: TAppColors.lightBackground,
+    surfaceTintColor: Colors.transparent,
+    elevation: 0,
+    scrolledUnderElevation: 0,
+    foregroundColor: TAppColors.lightOnSurface,
+    centerTitle: false,
+    titleTextStyle: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: TAppColors.lightOnSurface,
+    ),
+  );
+
   static AppBarTheme darkAppBarTheme() => const AppBarTheme(
-    backgroundColor: Colors.black,
-    elevation: 2,
-    foregroundColor: Colors.white,
-  ).copyWith();
+    backgroundColor: TAppColors.darkBackground,
+    surfaceTintColor: Colors.transparent,
+    elevation: 0,
+    scrolledUnderElevation: 0,
+    foregroundColor: TAppColors.darkOnSurface,
+    centerTitle: false,
+    titleTextStyle: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: TAppColors.darkOnSurface,
+    ),
+  );
 }
